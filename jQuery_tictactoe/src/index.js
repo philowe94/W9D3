81696cc65d64912b24767900a618d2884fc95116
Chris Joo <chrisj1225@gmail.com>
2021-04-21 14:34:04 -0400

    Finished View#setupBoard

--- jQuery_tictactoe/src/index.js
+++ jQuery_tictactoe/src/index.js
@@ -1,6 +1,11 @@
-const View = // require appropriate file
-const Game = // require appropriate file
+const View = require("./ttt-view");
+const Game = require("./game");
 
   $(() => {
     // Your code here
+    const g = new Game();
+    const $ttt = $(".ttt");
+    const tttView = new View(g, $ttt);
+    tttView.setupBoard();
+    console.log('test');
   });
--- jQuery_tictactoe/src/ttt-view.js
+++ jQuery_tictactoe/src/ttt-view.js
@@ -1,11 +1,37 @@
 class View {
-  constructor(game, $el) {}
-
+  constructor(game, $el) {
+    this.game = game;
+    this.$el = $el;
+  }
+    
   bindEvents() {}
 
   makeMove($square) {}
 
-  setupBoard() {}
+  setupBoard() {
+    let $grid = $("<ul>");
+    this.$el.append($grid);
+
+    $("ul").css("display", "flex");
+    $("ul").css("flex-wrap", "wrap");
+    $("ul").css("width", "300");
+    for (let i=0; i<9; i++) {
+      $("ul").append($("<li>").text("test"));
+    }
+
+    $("li").css("width", "90");
+    $("li").css("height", "90");
+    $("li").css("border", "2px solid black");
+    $("li").addClass("grey");
+
+    const toggleHover = (e) => {
+      const $li = $(e.currentTarget);
+      $li.toggleClass("yellow grey");
+    }
+    $("ul").on("mouseover", "li", toggleHover)
+    $("ul").on("mouseleave", "li", toggleHover)
+
+  }
 }
 
 module.exports = View;
